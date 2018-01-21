69b5682e5294f8f1cceb859f492114e28fbe5f31
Frank <frank@HaiboXudeMacBook-Pro.local>
2018-01-21 22:51:03 +0800

    update global variable for database ;
    :

---
+++ dbbox.py
@@ -0,0 +1,36 @@
+#-*- coding:utf-8 -*-
+import MySQLdb
+import sys
+
+class DB(object):
+    dbconn = None
+    dbcur = None
+    sqlQueue = []
+
+    def __init__(self, db_name, host_ip):
+        # type: () -> object
+        self.dbconn = MySQLdb.connect(
+            host=host_ip, user='root', passwd='root', port=3306, charset = 'utf8')
+        self.dbcur = self.dbconn.cursor()
+        self.dbconn.select_db(db_name)
+
+    def __del__(self):
+        self.dbcur.close()
+        self.dbconn.close()
+
+    def query_tmp_table(self):
+        self.dbcur.execute("SELECT id,type,name,url FROM tmp;")
+        rows = self.dbcur.fetchall()  # all rows in table
+        return rows
+
+    def query_film_table(self, param):
+        execute_sql = "SELECT name,link FROM resource_center WHERE name LIKE '%%%s%%' AND link!='';" %(param)
+        self.dbcur.execute(execute_sql)
+        rows = self.dbcur.fetchall()  # all rows in table
+        return rows
+
+    def query_type_table(self, type_param):
+        self.dbcur.execute(
+            "SELECT * FROM piaohua WHERE type='%s';" % (type_param))
+        rows = self.dbcur.fetchall()  # all rows in table
+        return rows
---
+++ main.py
@@ -0,0 +1,105 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import json
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+
+from tornado.options import define, options
+from dbbox import *
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#
+# sys.path.append("WeChat/modules/frank_wechat/session_services/")
+# import base_support as frank
+
+# from base_support import *
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=True, help="run in debug mode")
+
+db = DB('film', '127.0.0.1')
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r'/search', search_handler),
+            (r'/test', AjaxHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            "index.html",
+        )
+
+
+class AjaxHandler(tornado.web.RequestHandler):
+    db = None
+
+    # def __init__(self):
+    #     self.db = DB('film', '127.0.0.1')
+
+    # def search_film(self, param):
+    def post(self):
+        # self.write("hello world")
+        search_content = self.get_argument("message")
+        # self.db = DB('film', '127.0.0.1')
+        # rows = self.db.query_film_table(search_content)
+        rows = db.query_film_table(search_content)
+        namelist = []
+        for item,val in rows:
+            #item = item if item else 'nil'
+            if not item or not val:
+                continue
+            namelist.append({"name": item, "link": val})
+
+        filmdict = {}
+        filmdict['film'] = namelist
+        encode_json = json.dumps(filmdict)
+        #encode_json = '{"film":[{"name":1, "link":2}, {"name":3, "link":4}]}'
+        #self.get_argument("message")
+        self.write(encode_json)
+
+
+class search_handler(tornado.web.RequestHandler):
+    def post(self):
+        noun1 = self.get_argument('noun1')
+        noun2 = self.get_argument('noun2')
+        verb = self.get_argument('verb')
+        noun3 = self.get_argument('noun3')
+        self.render('poem.html', roads=noun1, wood=noun2, made=verb, difference=noun3)
+
+    def get(self):
+        keyword = self.get_argument('search_keyword')
+        rows = {}
+        if keyword:
+            self.db = DB('film', '127.0.0.1')
+            rows = self.db.query_film_table(keyword)
+            self.render(
+                "search.html", films=rows
+            )
+        else:
+            self.redirect('/')
+            # self.render(
+            #     "index.html",
+            # )
+
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    # options.port = 8888
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
